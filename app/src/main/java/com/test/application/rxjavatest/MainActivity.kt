8ff2104ca000cc4e39171aff290d7bfef3c7ca27
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2024-03-16 18:50:50 +0500

    - задача 3 Сделайте таймер. TextView которая раз в секунду меняется

--- app/src/main/java/com/test/application/rxjavatest/MainActivity.kt
+++ app/src/main/java/com/test/application/rxjavatest/MainActivity.kt
@@ -2,10 +2,34 @@ package com.test.application.rxjavatest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.tv_timer)
+
+        val timerDisposable = Observable.interval(1, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { seconds ->
+                textView.text = "Времени прошло: $seconds"
+            }
+
+        disposables.add(timerDisposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.clear()
     }
 }
