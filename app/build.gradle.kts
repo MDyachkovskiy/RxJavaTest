6479fb9f8d74e1c6a1467104764c1a10505f9d69
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2024-03-16 12:40:08 +0500

    - задача 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
 }
--- app/src/main/java/com/test/application/rxjavatest/MainActivity.kt
+++ app/src/main/java/com/test/application/rxjavatest/MainActivity.kt
@@ -2,10 +2,38 @@ package com.test.application.rxjavatest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.observables.ConnectableObservable
+import io.reactivex.rxjava3.subjects.PublishSubject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testPublishSubjectOne()
+        testPublishSubjectTwo()
+
+    }
+
+    private fun testPublishSubjectTwo() {
+        val subject = PublishSubject.create<String>()
+        val connectableObservable: ConnectableObservable<String> = subject.replay()
+
+        subject.onNext("1")
+        subject.onNext("2")
+        subject.onNext("3")
+
+        connectableObservable.connect()
+
+        connectableObservable.subscribe { Log.d("@@@", it) }
+    }
+
+    private fun testPublishSubjectOne() {
+        val subject = PublishSubject.create<String>()
+        subject.subscribe { Log.d("@@@", it) }
+
+        subject.onNext("1")
+        subject.onNext("2")
+        subject.onNext("3")
     }
 }
