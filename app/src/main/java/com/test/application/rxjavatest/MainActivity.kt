04b05e5026544582f00b251d485e911e42ee378a
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2024-03-20 12:26:44 +0500

    - задача из модуля корутин Напишите аналоги операторов throttleFisrt и throttleLatest из RxJava для Flow.

--- app/src/main/java/com/test/application/rxjavatest/MainActivity.kt
+++ app/src/main/java/com/test/application/rxjavatest/MainActivity.kt
@@ -2,10 +2,30 @@ package com.test.application.rxjavatest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val testFlow = flow {
+            repeat(100) { i ->
+                emit(i)
+                delay(10)
+            }
+        }
+        coroutineScope.launch {
+            testFlow
+                .throttleLast(300L)
+                .collect { value ->
+                    Log.d("@@@", "Emitted value: $value")
+                }
+        }
     }
 }
